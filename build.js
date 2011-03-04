b40e084477cf4240b207b1f81064f33532869427
Duzy Chan <code@duzy.info>
2011-03-04 15:23:06 +0800

    initial commit of UKI app builder

---
+++ build.js
@@ -0,0 +1,72 @@
+var cli    = require("cli");
+
+/*var flags =*/ cli.parse({
+    output:    ['o', 'Set compiled output file', 'string']
+});
+
+var fs     = require("fs"),
+    path   = require("path"),
+    sys    = require("sys");
+
+require.paths.unshift("/home/duzy/open/uki/src");
+require.paths.unshift(__dirname);
+
+var comp   = require("compiler");
+
+function compileFile(filePath, options) {
+    var code = comp.compile(filePath, options);
+    return code;
+}
+
+function makeHtmlCode(title, js) {
+    var code = '<!DOCTYPE html>';
+    code    += '<html><head><title>' + title + '</title></head>';
+    code    += '<html><body>'
+    code    += '<script lang="javascript">' + js + '</script>';
+    code    += '</body></html>'
+    return code;
+}
+
+cli.main(function(args, flags) {
+    if (args.length == 0 ) {
+	sys.error("No input file.");
+	return;
+    }
+
+    var outname = flags.output || "a.html";
+    var ext = path.extname(outname);
+    var name = path.basename(outname, ext);
+
+    var title = path.basename(args[0], path.extname(args[0]));
+
+    var codegen_options = {
+	ascii_only: false,
+	beautify: false,
+	indent_level: 4,
+	indent_start: 0,
+	quote_keys: false,
+	space_colon: false
+    };
+
+    var code = compileFile(args[0], codegen_options);
+    if (!code) {
+	sys.error('No code generated.');
+	return;
+    }
+
+    switch(ext.toLowerCase()) {
+    case ".js":
+	if (outname == path.basename(args[0])) {
+	    name = name + ".compiled"
+	}
+	break;
+    case ".html":
+	code = makeHtmlCode(title, code);
+	break;
+    default:
+	sys.error('Unsupported extname "' + ext + '".');
+	return;
+    }
+
+    fs.writeFileSync(name + ext, code, "utf8");
+});
---
+++ test.js
@@ -0,0 +1,49 @@
+var uki = require("uki");
+
+uki([
+    
+    {
+        view: 'Button', pos: 'l:50% t:40px w:200px ml:-100px',
+        label: 'Normal Button', tabIndex: 1, on: {
+            click: function() {
+                alert('Inline click handler for ' + this.label());
+            }
+        }
+    },
+    {
+        view: 'Button', pos: 'l:50% t:75px w:200px ml:-100px',
+        label: 'Confirm Button', tabIndex: 1, confirm: true
+    },
+    {
+        view: 'Button', pos: 'l:50% t:110px w:200px ml:-100px',
+        label: 'With Icon', tabIndex: 1, iconSrc: './settings.png'
+    },
+    {
+        view: 'Button', pos: 'l:50% t:145px w:40px ml:-20px',
+        tabIndex: 1, iconSrc: './settings.png'
+    },
+    
+    {
+        view: 'Button', pos: 'l:50% t:200px w:200px ml:-100px',
+        label: 'Disabled Button', tabIndex: 1, disabled: true
+    },
+    {
+        view: 'Button', pos: 'l:50% t:235px w:200px ml:-100px',
+        label: 'Disabled Confirm Button', tabIndex: 1, confirm: true,
+        disabled: true
+    },
+    {
+        view: 'Button', pos: 'l:50% t:270px w:200px ml:-100px',
+        label: 'Disabled With Icon', tabIndex: 1, iconSrc: './settings.png',
+        disabled: true
+    },
+    {
+        view: 'Button', pos: 'l:50% t:305px w:40px ml:-20px',
+        tabIndex: 1, iconSrc: './settings.png', disabled: true
+    }
+    
+]).attach();
+
+uki('Button:gt(0)').on('click', function() {
+    alert('Selector click handler for ' + this.label());
+});
