c83df5e5cb0ab2d5427d7c58cb69f6e4ffd81edc
Duzy Chan <code@duzy.info>
2011-03-11 19:31:58 +0800

    refactoring

--- ui/core/view/base.js
+++ ui/core/view/base.js
@@ -6,159 +6,6 @@ var view  = require('../view'),
     dom   = require('../dom'),
     event = require('../event');
 
-var Base = new Object.Class({
-    typeName: 'Base',
-
-    init: function(initArgs) {
-    },
-
-    $create: function(initArgs) {
-        this._createDom(initArgs);
-        this.dom()[env.expando] = this.dom()[env.expando] || env.guid++;
-        view.register(this);
-    }
-});
-
-var proto = Base.prototype;
-
-proto.destruct = function() {
-    view.unregisterId(this);
-    view.unregister(this);
-    this.removeListener();
-    this.destructed = true;
-};
-
-/**#@+ @memberOf view.Base# */
-
-proto._createDom = function(initArgs) {
-    this._dom = dom.createElement(initArgs.tagName || 'div');
-};
-
-/**
- * Called when view was resized
- */
-proto.resized = fun.FS;
-
-/**
- * Get views container dom node.
- * @returns {Element} dom
- */
-proto.dom = function() { return this._dom; };
-
-
-fun.delegateProp(proto, 'html', '_dom', 'innerHTML');
-
-proto.text = function(v) {
-    return this.html(v && dom.escapeHTML(v));
-};
-
-/* ------------------------------- Common settings --------------------------------*/
-/**
- * Used for fast (on hash lookup) view searches: build('#view_id');
- *
- * @param {string=} id New id value
- * @returns {string|view.Base} current id or self
- */
-proto.id = function(id) {
-    if (id === undefined) { return this.dom().id; }
-    if (this.dom().id) { view.unregisterId(this); }
-    this.dom().id = id;
-    view.registerId(this);
-    return this;
-};
-
-/**
- * Accessor for dom().className
- * @param {string=} className
- *
- * @returns {string|view.Base} className or self
- */
-fun.delegateProp(proto, 'className', 'dom');
-
-proto.addClass = function(className) {
-    dom.addClass(this.dom(), className);
-    return this;
-};
-
-proto.hasClass = function(className) {
-    return dom.hasClass(this.dom(), className);
-};
-
-proto.removeClass = function(className) {
-    dom.removeClass(this.dom(), className);
-    return this;
-};
-
-proto.toggleClass = function(className, condition) {
-    dom.toggleClass(this.dom(), className, condition);
-    return this;
-};
-
-
-
-
-proto.domForEvent = function(type) {
-    return this.dom();
-};
-
-/**
- * @param {String} name Event name, or space separated names
- * @param {function()} callback
- */
-proto.addListener = function(names, callback) {
-    if (typeof names === 'object') {
-	names.forEach(function(callback, name) {
-	    this.addListener(name, callback);
-	}, this);
-    } else {
-	var wrapper = callback.bindOnce(this);
-	this._eventNames || (this._eventNames = {});
-	names.split(' ').forEach(function(name) {
-	    this._eventNames[name] = true;
-	    event.addListener(this.domForEvent(name), name, wrapper);
-	}, this);
-    }
-    return this;
-};
-
-/**
- * @param {String} name Event name, or space separated names,
- *                      or null to remove from all types
- * @param {function()} callback or null to remove all callbacks
- */
-proto.removeListener = function(names, callback) {
-    var wrapper = callback && callback.bindOnce(this);
-    names || (names = Object.keys((this._eventNames || {})).join(' '));
-    names.split(' ').forEach(function(name) {
-	event.removeListener(this.domForEvent(name), name, wrapper);
-    }, this);
-    return this;
-};
-
-proto.trigger = function(e) {
-    var node = this.domForEvent(e.type);
-    var wrapped = event.createEvent(e, { target: node });
-    return event.trigger.call(node, e);
-};
-
-proto.on = proto.addListener;
-proto.emit = proto.trigger;
-
-
-/**
- * Shortcut to set absolute positioning props
- * @param {string|object} p Position string in the form of
- *                          'l:10px t:10px r:30% h:200px'
- */
-proto.pos = function(pos) {
-    if (pos === undefined) {
-	return this._styleToPos(this.dom().style);
-    }
-    pos = this._expandPos(pos);
-    this._applyPosToStyle(pos, this.dom().style);
-    return this;
-};
-
 var rules = [
     'top', 'right', 'left', 'bottom', 'width', 'height',
     'marginLeft', 'marginTop', 'marginBottom', 'marginRight'
@@ -171,37 +18,172 @@ ruleMap = {
     mt: 'marginTop', mb: 'marginBottom'
 };
 
-proto._styleToPos = function(style) {
-    var res = {};
-    rules.forEach(function(rule) {
-	if (style[rule]) {
-	    res[rule] = style[rule];
+var Base = new Object.Class({
+    typeName: 'Base',
+
+    init: function(initArgs) {
+    },
+
+    $create: function(initArgs) {
+        this._createDom(initArgs);
+        this.dom()[env.expando] = this.dom()[env.expando] || env.guid++;
+        view.register(this);
+    },
+
+    destruct: function() {
+	view.unregisterId(this);
+	view.unregister(this);
+	this.removeListener();
+	this.destructed = true;
+    },
+
+    _createDom: function(initArgs) {
+	this._dom = dom.createElement(initArgs.tagName || 'div');
+    },
+
+    /**
+     * Called when view was resized
+     */
+    resized: fun.FS,
+
+    /**
+     * Get views container dom node.
+     * @returns {Element} dom
+     */
+    dom: function() { return this._dom; },
+
+    text: function(v) {	return this.html(v && dom.escapeHTML(v)); },
+
+    /* ----------------------- Common settings --------------------------------*/
+    /**
+ * Used for fast (on hash lookup) view searches: build('#view_id');
+ *
+ * @param {string=} id New id value
+ * @returns {string|view.Base} current id or self
+ */
+    id: function(id) {
+	if (id === undefined) { return this.dom().id; }
+	if (this.dom().id) { view.unregisterId(this); }
+	this.dom().id = id;
+	view.registerId(this);
+	return this;
+    },
+
+    /**
+ * Accessor for dom().className
+ * @param {string=} className
+ *
+ * @returns {string|view.Base} className or self
+ */
+
+    addClass: function(className) {
+	dom.addClass(this.dom(), className);
+	return this;
+    },
+
+    hasClass: function(className) {
+	return dom.hasClass(this.dom(), className);
+    },
+
+    removeClass: function(className) {
+	dom.removeClass(this.dom(), className);
+	return this;
+    },
+
+    toggleClass: function(className, condition) {
+	dom.toggleClass(this.dom(), className, condition);
+	return this;
+    },
+
+    domForEvent: function(type) {
+	return this.dom();
+    },
+
+/**
+ * @param {String} name Event name, or space separated names
+ * @param {function()} callback
+ */
+    addListener: function(names, callback) {
+	if (typeof names === 'object') {
+	    names.forEach(function(callback, name) {
+		this.addListener(name, callback);
+	    }, this);
+	} else {
+	    var wrapper = callback.bindOnce(this);
+	    this._eventNames || (this._eventNames = {});
+	    names.split(' ').forEach(function(name) {
+		this._eventNames[name] = true;
+		event.addListener(this.domForEvent(name), name, wrapper);
+	    }, this);
 	}
-    });
-    return res;
-};
+	return this;
+    },
 
-proto._expandPos = function(pos) {
-    if (typeof pos === 'string') {
-	var p = pos;
-	pos = {};
-	p.split(/\s+/).forEach(function(rule) {
-	    var parts = rule.split(':');
-	    pos[parts[0]] = parts[1];
+/**
+ * @param {String} name Event name, or space separated names,
+ *                      or null to remove from all types
+ * @param {function()} callback or null to remove all callbacks
+ */
+    removeListener: function(names, callback) {
+	var wrapper = callback && callback.bindOnce(this);
+	names || (names = Object.keys((this._eventNames || {})).join(' '));
+	names.split(' ').forEach(function(name) {
+	    event.removeListener(this.domForEvent(name), name, wrapper);
+	}, this);
+	return this;
+    },
+
+    trigger: function(e) {
+	var node = this.domForEvent(e.type);
+	var wrapped = event.createEvent(e, { target: node });
+	return event.trigger.call(node, e);
+    },
+
+    /**
+ * Shortcut to set absolute positioning props
+ * @param {string|object} p Position string in the form of
+ *                          'l:10px t:10px r:30% h:200px'
+ */
+    pos: function(pos) {
+	if (pos === undefined) {
+	    return this._styleToPos(this.dom().style);
+	}
+	pos = this._expandPos(pos);
+	this._applyPosToStyle(pos, this.dom().style);
+	return this;
+    },
+
+    _styleToPos: function(style) {
+	var res = {};
+	rules.forEach(function(rule) {
+	    if (style[rule]) {
+		res[rule] = style[rule];
+	    }
 	});
-    }
-    ruleMap && ruleMap.forEach(function(longRule, shortRule) {
-	if (pos[shortRule]) pos[longRule] = pos[shortRule];
-    });
-    return pos;
-};
+	return res;
+    },
 
-proto._applyPosToStyle = function(pos, style) {
-    style.position = 'absolute';
-    rules && rules.forEach(function(rule) {
-	style[rule] = pos[rule] || '';
-    });
-};
+    _expandPos: function(pos) {
+	if (typeof pos === 'string') {
+	    var p = pos;
+	    pos = {};
+	    p.split(/\s+/).forEach(function(rule) {
+		var parts = rule.split(':');
+		pos[parts[0]] = parts[1];
+	    });
+	}
+	ruleMap && ruleMap.forEach(function(longRule, shortRule) {
+	    if (pos[shortRule]) pos[longRule] = pos[shortRule];
+	});
+	return pos;
+    },
+
+    _applyPosToStyle: function(pos, style) {
+	style.position = 'absolute';
+	rules && rules.forEach(function(rule) {
+	    style[rule] = pos[rule] || '';
+	});
+    },
 
 /**
  * Accessor for view visibility.
@@ -209,21 +191,21 @@ proto._applyPosToStyle = function(pos, style) {
  * @param {boolean=} state
  * @returns {boolean|view.Base} current visibility state of self
  */
-proto.visible = function(state) {
-    if (state === undefined) {
-	return this.dom().style.display != 'none';
-    }
-
-    var origState = this.visible();
-    this.dom().style.display = state ? '' : 'none';
-
-    // if we change from invis to vis, and we have dom, and we're attached
-    // redraw
-    if (state && !origState && this.dom() && this.dom().offsetWidth) {
-	this.resized();
-    }
-    return this;
-};
+    visible: function(state) {
+	if (state === undefined) {
+	    return this.dom().style.display != 'none';
+	}
+	
+	var origState = this.visible();
+	this.dom().style.display = state ? '' : 'none';
+	
+	// if we change from invis to vis, and we have dom, and we're attached
+	// redraw
+	if (state && !origState && this.dom() && this.dom().offsetWidth) {
+	    this.resized();
+	}
+	return this;
+    },
 
 /* ----------------------------- Container api ------------------------------*/
 
@@ -234,52 +216,58 @@ proto.visible = function(state) {
  * @param {?view.Base=} parent
  * @returns {view.Base} parent or self
  */
-proto.parent = function(parent) {
-    if (parent === undefined) {
-	return this._parent;
-    }
+    parent: function(parent) {
+	if (parent === undefined) {
+	    return this._parent;
+	}
 
-    this._parent = parent;
-    return this;
-};
+	this._parent = parent;
+	return this;
+    },
 
 /**
  * Reader for previous view
  * @returns {view.Base}
  */
-proto.prevView = function() {
-    if (!this.parent()) { return null; }
-    return this.parent().childViews()[this._viewIndex - 1] || null;
-};
+    prevView: function() {
+	if (!this.parent()) { return null; }
+	return this.parent().childViews()[this._viewIndex - 1] || null;
+    },
 
-/**
+    /**
  * Reader for next view
  * @returns {view.Base}
  */
-proto.nextView = function() {
-    if (!this.parent()) { return null; }
-    return this.parent().childViews()[this._viewIndex + 1] || null;
-};
+    nextView: function() {
+	if (!this.parent()) { return null; }
+	return this.parent().childViews()[this._viewIndex + 1] || null;
+    },
 
+    scroll: function(dx, dy) {
+	dx && this.scrollLeft(this.scrollLeft() + dx);
+	dy && this.scrollTop(this.scrollTop() + dy);
+    },
+
+    /* ----------------------------- Layout ------------------------------*/
+    /**
+     */
+    clientRect: function(ignoreScroll) {
+	return dom.clientRect(this.dom(), ignoreScroll);
+    },
+
+    childViews: function() {
+	return [];
+    },
+    
+});
+
+var proto = Base.prototype;
+fun.delegateProp(proto, 'html', '_dom', 'innerHTML');
+fun.delegateProp(proto, 'className', 'dom');
 fun.delegateProp(proto, ['scrollTop', 'scrollLeft', 'title', 'alt'], 'dom');
 
-proto.scroll = function(dx, dy) {
-    dx && this.scrollLeft(this.scrollLeft() + dx);
-    dy && this.scrollTop(this.scrollTop() + dy);
-};
-
-/* ----------------------------- Layout ------------------------------*/
-
-/**
- */
-proto.clientRect = function(ignoreScroll) {
-    return dom.clientRect(this.dom(), ignoreScroll);
-};
-
-
-proto.childViews = function() {
-    return [];
-};
+proto.on = proto.addListener,
+proto.emit = proto.trigger,
 
 exports.Base = Base;
 //})();
